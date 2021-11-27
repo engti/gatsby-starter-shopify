5f5ad1abcf855478a09a3734c1f84e0bbfedb68f
Robin Browne <rbrowne@gmail.com>
2021-11-27 21:34:51 +0530

    site url updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     siteTitle: "gatsby-starter-shopify",
     siteTitleDefault: "gatsby-starter-shopify by @GatsbyJS",
-    siteUrl: "https://shopify-demo.gatsbyjs.com",
+    siteUrl: "https://shop.datasea.in",
     hrefLang: "en",
     siteDescription:
       "A Gatsby starter using the latest Shopify plugin showcasing a store with product overview, individual product pages, and a cart.",
